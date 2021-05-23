1e51374ef837a7a3c056a160e5a3aa0db2a453c6
gorushkin <gorushkin@gmail.com>
2021-05-23 14:35:50 +0300

    relocate

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,3 +1,13 @@
-const getAll = async () => [];
+import path from 'path';
+import fs from 'fs';
+
+const pathToData = path.join(process.cwd(), process.env.DB);
+
+const getData = async (fieldName = 'users') =>
+  JSON.parse(await fs.promises.readFile(pathToData, 'utf-8'))[fieldName];
+
+getData();
+
+const getAll = async () => getData('users');
 
 export { getAll };
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -2,12 +2,18 @@ import express from 'express';
 import User from './user.model.js';
 import * as usersService from './user.service.js';
 
-
 const router = express.Router();
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
   res.json(users.map(User.toResponse));
+});
+
+router.route('/').post(async (req, res) => {
+  const { name, login, password } = req.body;
+  const user = new User({ name, login, password });
+  console.log('user: ', User.toResponse(user));
+  res.json(User.toResponse(user));
 });
 
 export default router;
